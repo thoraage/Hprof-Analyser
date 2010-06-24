fcedebd2bca1a59648a6da9f0e3885517b7d0734
thoraage <thoraageeldby@gmail.com>
2010-06-25 01:17:32 +0200

    More

--- src/main/scala/com/arktekk/hprofreader/HprofAnalyse.scala
+++ src/main/scala/com/arktekk/hprofreader/HprofAnalyse.scala
@@ -1,6 +1,5 @@
 package com.arktekk.hprofreader
 
-import java.lang.String
 import java.io.File
 import com.sun.tools.hat.internal.parser.Reader
 import com.sun.tools.hat.internal.model._
@@ -24,24 +23,11 @@ object HprofAnalyse {
       } else {
         val snapshot = Reader.readFile("/Users/thoraageeldby/Documents/Arktekk/Sources/callway/dump.hprof", true, 0)
         snapshot.resolve(true)
-        var mine = new Wrapper(snapshot.getClasses.asInstanceOf[java.util.Iterator[JavaClass]]).filter(_.asInstanceOf[JavaClass].getName.startsWith("com.tandberg"))
-        mine.next.visitReferencedObjects(new JavaHeapObjectVisitor() {
-          override def visit(javaHeapObject: JavaHeapObject) {
-            println(javaHeapObject.getClazz.getName + "[" + javaHeapObject.getId + "]: ")
-            new RichEnumeration(javaHeapObject.getReferers.asInstanceOf[java.util.Enumeration[JavaHeapObject]])
-                    .filter(!_.getClazz.getName.startsWith("java"))
-                    .map("RefferedBy: " + _.getClazz.getName)
-                    .foreach(println _)
-          }
-          override def exclude(javaClass: JavaClass, javaField: JavaField): Boolean = {
-            println("exclude");
-            false
-          }
-          override def mightExclude: Boolean = {
-            println("mightexclude");
-            false
-          }
-        })
+        val interestPath = "com.tandberg"
+        var mine = new Wrapper(snapshot.getClasses.asInstanceOf[java.util.Iterator[JavaClass]]).filter(_.asInstanceOf[JavaClass].getName.startsWith(interestPath))
+        var clazz = mine.next
+
+        checkReferencesAndPrintResult(clazz.getInstances(false).nextElement.asInstanceOf[JavaObject])
         println("Analysed");
       }
     }
@@ -49,7 +35,46 @@ object HprofAnalyse {
 
   class RichEnumeration[T](enumeration: java.util.Enumeration[T]) extends Iterator[T] {
     def hasNext: Boolean = enumeration.hasMoreElements()
+
     def next: T = enumeration.nextElement()
+  }
+
+  object BreakOffReason extends Enumeration {
+    val InfiniteLoop = Value("InfiniteLoop")
+    val NoReferers = Value("NoReferers")
+    val MaxDepthExceeded = Value("MaxDepthExceeded")
+  }
+
+  case class InstanceStack(stack: List[JavaLazyReadObject], reason: BreakOffReason.Value) {
+    override def toString = reason + ": " + stack.foldRight("") {(obj, string) => obj.getClazz.getName + "(" + obj.getId + ") -> "}
+  }
+
+  def checkReferences(obj: JavaLazyReadObject, depth: Int, visited: List[JavaLazyReadObject]): List[InstanceStack] = {
+    //val prepend = if (depth == 0) "" else String.format("%" + (depth * 2) + "s", "")
+    //println(prepend + "Class: " + obj.getClazz.getName + ", ref: " + obj.getId)
+    if (!visited.forall {_.getId != obj.getId}) {
+      //error("Object already visited")
+      new InstanceStack(visited, BreakOffReason.InfiniteLoop) :: Nil
+    } else if (depth > 50) {
+      new InstanceStack(visited, BreakOffReason.MaxDepthExceeded) :: Nil
+    } else {
+      val referers = new RichEnumeration(obj.getReferers)
+      if (referers.hasNext) {
+        referers.foldRight(List[InstanceStack]()) {
+          (ref, instanceStacks) =>
+            checkReferences(ref.asInstanceOf[JavaLazyReadObject], depth + 1, obj :: visited) ::: instanceStacks
+        }
+        /*while (referers.hasNext) {
+          checkReferences(referers.next.asInstanceOf[JavaLazyReadObject], depth + 1, obj :: visited)
+        }*/
+      } else {
+        new InstanceStack(visited, BreakOffReason.NoReferers) :: Nil
+      }
+    }
+  }
+
+  def checkReferencesAndPrintResult(obj: JavaLazyReadObject): Unit = {
+    checkReferences(obj, 0, Nil).foreach {println _}
   }
 
 }
